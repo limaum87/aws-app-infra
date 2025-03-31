15709d2ca6d6fb84efbb51baa70b14957b485e73
Felipe Lima <limaum87@gmail.com>
2025-03-30 22:50:52 -0300

    Alteracao no nat gateway

--- modules/nat_gateway/main.tf
+++ modules/nat_gateway/main.tf
@@ -23,8 +23,3 @@ resource "aws_route_table" "private" {
   tags = var.tags
 }
 
-# Associação da Tabela de Rotas Privada com a Subnet Privada
-resource "aws_route_table_association" "private" {
-  subnet_id      = var.private_subnet_id
-  route_table_id = aws_route_table.private.id
-}
